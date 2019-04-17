d853b02ac85ce8c0c265d16785094bcf40d57977
Stéphane Díaz-Alejo León <ragnarokshadows99@gmail.com>
2019-04-17 12:12:37 +0200

    Vista de appointments fix

--- src/managementapplication/AppTableViewController.java
+++ src/managementapplication/AppTableViewController.java
@@ -8,6 +8,7 @@ package managementapplication;
 import DBAccess.ClinicDBAccess;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.beans.binding.Bindings;
